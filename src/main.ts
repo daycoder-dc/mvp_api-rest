51dfc49590abdad663765eba00edc246fd7c5e10
Dairo Carrasquilla <dcarrasquilla@ises.com.co>
2026-04-10 12:59:39 -0500

    chore: avance main

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
-import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from '@/app/app.module';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    abortOnError: false,
+    autoFlushLogs: true,
+    bufferLogs: true,
+    rawBody: true
+  });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(parseInt(process.env.PORT!), () => {
+    console.log("------------------------------------------");
+    console.log(`  🟢 API SERVICE RUNNING 🚀`);
+    console.log(`  🔗 URL: http://localhost:${process.env.PORT}`);
+    console.log("------------------------------------------");
+  });
 }
 
 bootstrap();
