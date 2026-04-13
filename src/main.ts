764ae61cad53a1aca3b78db31da851201332dac1
Dairo Carrasquilla <cdairo2204@gmail.com>
2026-04-12 21:23:25 -0500

    chore: avance conf main

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { VersioningType } from '@nestjs/common';
 import { AppModule } from '@/app/app.module';
 import { NestFactory } from '@nestjs/core';
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
+import { HttpExceptionFilter } from './config/http-exception';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -10,11 +13,36 @@ async function bootstrap() {
     rawBody: true
   });
 
+  app.set("trust proxy", 1);
+  app.setGlobalPrefix("api");
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+    prefix: "v"
+  });
+
+  app.enableCors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+    credentials: true
+  });
+
+  app.useLogger(app.get(Logger));
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter);
+  app.useBodyParser("urlencoded", { extends: true });
+  app.useBodyParser("json", { limit: "50mb" });
+
+
   await app.listen(parseInt(process.env.PORT!), () => {
-    console.log("------------------------------------------");
-    console.log(`  🟢 API SERVICE RUNNING 🚀`);
-    console.log(`  🔗 URL: http://localhost:${process.env.PORT}`);
-    console.log("------------------------------------------");
+    console.log("");
+    console.log("-------------------------------------------------");
+    console.log(` 🟢 API SERVICE RUNNING 🚀`);
+    console.log(` 🔗 URL: http://localhost:${process.env.PORT}`);
+    console.log("-------------------------------------------------");
+    console.log("");
   });
 }
 
